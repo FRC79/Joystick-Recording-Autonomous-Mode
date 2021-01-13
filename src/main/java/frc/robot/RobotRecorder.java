75d440715d30f20c478f39ce591d0687fa7cb606
Scott McKool <44004555+ScottMcKaughan@users.noreply.github.com>
2021-01-13 08:52:22 -0500

    Imported constants from Constants.java

--- src/main/java/frc/robot/RobotRecorder.java
+++ src/main/java/frc/robot/RobotRecorder.java
@@ -1,14 +1,15 @@
 
-// inport constants from robotMap
+// inport constants from constants.java
+import frc.robot.Constants.RobotRecorderConstants;
 
 public class RobotRecorder {
 
-    // inport constants from robotMap
-    static final double  SAVE_FREQ      = // constant
-    static final double  AUTO_LENGTH    = // constant 
-    static final String  FILE_EXT       = // constant
-    static final String  FILE_PATH      = // constant
-    static final boolean PRINT_DEBUG    = // constant
+    // inport constants from constants.java
+    static final double  SAVE_FREQ      = RobotRecorderConstants.RECORDING_FREQUENCY;
+    static final double  AUTO_LENGTH    = RobotRecorderConstants.AUTONOMOUS_DURATION; 
+    static final String  FILE_EXT       = RobotRecorderConstants.SAVE_FILE_EXTENTION;
+    static final String  FILE_PATH      = RobotRecorderConstants.SAVE_FILE_PATH;
+    static final boolean PRINT_DEBUG    = RobotRecorderConstants.PRINT_DEBUG_INFO;
 
     private int curUpdateIndex;
 
