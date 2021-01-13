98001a77581ba8b4a64a67eb9a68d9a6a336bddf
Scott McKool <44004555+ScottMcKaughan@users.noreply.github.com>
2021-01-13 08:46:30 -0500

    Create Constants.java

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,13 @@
+
+package frc.robot;
+
+public final class Constants {
+  public static final class RobotRecorderConstants{
+    
+    public static final double RECORDING_FREQUENCY  = -1 // miliseconds
+    public static final double AUTONOMOUS_DURATION  = -1 // seconds
+    public static final String SAVE_FILE_EXTENSION  = ".lol"
+    public static final String SAVE_FILE_PATH       = "TBD"
+    public static final boolean PRINT_DEBUG_INFO    = false;
+  }
+}
