2aff4db4a28d41faf81810de118febbee37c316e
Scott McKool <44004555+ScottMcKaughan@users.noreply.github.com>
2021-01-22 09:00:49 -0500

    added verbose option and comments

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,10 +4,11 @@ package frc.robot;
 public final class Constants {
   public static final class RobotRecorderConstants{
     
-    public static final double RECORDING_FREQUENCY  = -1 // miliseconds
-    public static final double AUTONOMOUS_DURATION  = -1 // seconds
-    public static final String SAVE_FILE_EXTENSION  = ".lol"
-    public static final String SAVE_FILE_PATH       = "TBD"
-    public static final boolean PRINT_DEBUG_INFO    = false;
+    public static final double RECORDING_FREQUENCY  = -1      // miliseconds (maybe change to microseconds)
+    public static final double AUTONOMOUS_DURATION  = -1      // seconds
+    public static final String SAVE_FILE_EXTENSION  = ".lol"  // extension for files that robotArrays are saved in
+    public static final String SAVE_FILE_PATH       = "TBD"   // path on the roborio to keep robotArray files
+    public static final boolean PRINT_DEBUG_INFO    = true;   // true to print info about the recoring and playback
+    public static final boolean VERBOSE_DEBUG_PRINT = true;   // true to print a lot of in depth info about recording and playback
   }
 }
