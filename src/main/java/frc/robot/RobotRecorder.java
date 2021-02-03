c3bca12eb9f9b3a95a1601a2dcc99211348919b8
scott-mckool <hydrogen.poo@gmail.com>
2021-02-02 19:11:17 -0500

    Corrected syntax mistakes

--- src/main/java/frc/robot/RobotRecorder.java
+++ src/main/java/frc/robot/RobotRecorder.java
@@ -20,7 +20,7 @@ use the sendable chooser to let drivers choose what file to read from?
 import frc.robot.Constants.RobotRecorderConstants;
 
 // use an arraylist of hashMaps for storing the data about the robot
-import java.util.Arraylist;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class RobotRecorder {
@@ -42,9 +42,13 @@ public class RobotRecorder {
     // gets saved and cleared every update()
     private RobotState curState;
 
-    private int curUpdateIndex; // current ID for the robot's State in the arraylist, for playback 
+    /* current ID for the robot's State in the arraylist, for playback */
+    private int curUpdateIndex; 
     
-    private double curTime = System.currentTimeMillis(); // current time for update() (milliseconds) (might make this microseconds)
+    /* current time for update() (milliseconds) (might make this microseconds) */
+    private double curTime = System.currentTimeMillis(); 
+    /* last time update() ran */
+    private double lastUpdate;
 
     // the modes of operation for the robotRecorder
     enum Mode{
@@ -64,7 +68,7 @@ public class RobotRecorder {
         private static final long serialVersionUID = -1L; 
         
         /* the keys and vlues that actually describe the robot */
-        public HashMap<String, double> valueMap = new HashMap<String, double>();
+        public HashMap<String, Double> valueMap = new HashMap<String, Double>();
         
         /* need to overwrite toString, used in the serialization process */
         @Override
@@ -75,23 +79,23 @@ public class RobotRecorder {
         }
     }
     
-    public StartPlayback(){
+    public void startPlayback(){
         curMode = Mode.PLAY;
         curUpdateIndex = 0;
         // grab recordArray from a file or something
     }
     
-    public stopPlaying(){
+    public void stopPlaying(){
         curMode = Mode.NORMAL;
     }
 
-    public StartRecording(){
-        recordArray = new ArrayList<HashMap<String, double>>();
+    public void startRecording(){
+        recordArray = new ArrayList<RobotState>();
         startTime = System.currentTimeMillis()*1000;
         curMode = Mode.RECORD;
     }
     
-    public stopRecording(){
+    public void stopRecording(){
         curMode = Mode.NORMAL;
         // save recordArray to a file
     }
@@ -101,7 +105,7 @@ public class RobotRecorder {
      * @param Value the value sto be stored at this point in the record, must be a double
      * will store a value under a unique key at the current point in the robot record, only works when the robot is in recording mode.
      */
-    public SetRobotData(String Key, double Value){
+    public void SetRobotData(String Key, double Value){
         if(curMode == Mode.RECORD & curUpdateIndex < recordArray.size()){
             curState.put(Key,Value); // at the current state, put this key value pair in the hashMap
         }
@@ -119,16 +123,16 @@ public class RobotRecorder {
         return null;
     }
 
-    private update(){
+    private void update(){
         
-        if( (curTime - lastUpdate) >= updateFrequency){ // after the given time frequency
+        if( (curTime - lastUpdate) >= UPDATE_FREQ){ // after the given time frequency
             if(curMode == Mode.PLAY){ // when playing back info
 
                 if(curUpdateIndex > recordArray.size()){ // stop when out of instructions to follow
 
                     if(PRINT_DEBUG){ System.out.println("playback over, out of instructions"); }
                     if(PRINT_DEBUG && VERBOSE_DEBUG){ // in depth info about the end of playback
-                        System.out.println("Recording End info:"+recordArray.length+"instructions"); 
+                        System.out.println("Recording End info:"+recordArray.size()+"instructions"); 
                     }
                     stopPlaying();
                     return;
@@ -141,13 +145,13 @@ public class RobotRecorder {
 
                     if(PRINT_DEBUG){ System.out.println("Recording over, autonomous timer expired"); }
                     if(PRINT_DEBUG && VERBOSE_DEBUG){  // in depth info about the end of recording
-                        System.out.println("Recording End info:"+recordArray.length+"instructions"); 
+                        System.out.println("Recording End info:"+recordArray.size()+"instructions"); 
                     }
                     stopRecording();
                     return;
                 }
-                recordArray.Add(curState); // save current state to array
-                curState.clear() // clear state for next go around
+                recordArray.add(curState); // save current state to array
+                curState.clear(); // clear state for next go around
             }
             lastUpdate = curTime; // set lastUpdate to reset the timer 
         }
