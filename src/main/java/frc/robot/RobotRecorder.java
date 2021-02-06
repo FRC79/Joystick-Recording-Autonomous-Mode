c5c566eb6897129e7f60c1e3504f0b36331735b9
scott-mckool <hydrogen.poo@gmail.com>
2021-02-06 00:13:34 -0500

    fixed var names

--- src/main/java/frc/robot/RobotRecorder.java
+++ src/main/java/frc/robot/RobotRecorder.java
@@ -32,9 +32,9 @@ import java.io.ObjectOutputStream;
 public class RobotRecorder {
 
     // import constants from constants.java
-    static final double  UPDATE_FREQ    = RobotRecorderConstants.UPDATE_FREQUENCY;
+    static final double  UPDATE_FREQ    = RobotRecorderConstants.RECORDING_FREQUENCY;
     static final double  AUTO_LENGTH    = RobotRecorderConstants.AUTONOMOUS_DURATION; 
-    static final String  FILE_EXT       = RobotRecorderConstants.SAVE_FILE_EXTENTION;
+    static final String  FILE_EXT       = RobotRecorderConstants.SAVE_FILE_EXTENSION;
     static final String  FILE_PATH      = RobotRecorderConstants.SAVE_FILE_PATH;
     static final String  FILE_NAME      = RobotRecorderConstants.SAVE_FILE_NAME;
     static final boolean PRINT_DEBUG    = RobotRecorderConstants.PRINT_DEBUG_INFO;
