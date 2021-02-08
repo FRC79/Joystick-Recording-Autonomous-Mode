8ed9bdd9caf09ff5f8c467fed71cff85a66bd98a
Scott McKool <hydrogen.poo@gmail.com>
2021-02-08 08:32:04 -0500

    Update Constants.java

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -4,9 +4,9 @@ package frc.robot;
 public final class Constants {
   public static final class RobotRecorderConstants{
     
-    public static final double  RECORDING_FREQUENCY   = -1;     // miliseconds (maybe change to microseconds)
-    public static final double  AUTONOMOUS_DURATION   = -1;     // seconds
-    public static final String  SAVE_FILE_EXTENSION   = ".lol"; // extension for files that robotArrays are saved in
+    public static final double  RECORDING_FREQUENCY   = 15;     // miliseconds (maybe change to microseconds)
+    public static final double  AUTONOMOUS_DURATION   = 5;      // seconds
+    public static final String  SAVE_FILE_EXTENSION   = ".lmao";// extension for files that robotArrays are saved in
     public static final String  SAVE_FILE_PATH        = "TBD";  // path on the roborio to keep robotArray files( not used )
     public static final String  SAVE_FILE_NAME        = "test"; // name of the file to make or read
     public static final boolean PRINT_DEBUG_INFO      = true;   // true to print info about the recoring and playback
