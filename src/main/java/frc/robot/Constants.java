c07b918c1fde18867cc62f062de373fcfd4cf829
Scott McKool <44004555+Scott-McKool@users.noreply.github.com>
2021-02-22 08:40:29 -0500

    Fixed save path

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -7,7 +7,7 @@ public final class Constants {
     public static final double  RECORDING_FREQUENCY   = 15;     // miliseconds (maybe change to microseconds)
     public static final double  AUTONOMOUS_DURATION   = 5;      // seconds
     public static final String  SAVE_FILE_EXTENSION   = ".lmao";// extension for files that robotArrays are saved in
-    public static final String  SAVE_FILE_PATH        = "/home/lvuser/robotRecordings/";// path on the roborio to keep robotArray files( thanks for ruining the cool looking code )
+    public static final String  SAVE_FILE_PATH        = "/home/lvuser/";// path on the roborio to keep robotArray files( thanks for ruining the cool looking code )
     public static final String  SAVE_FILE_NAME        = "test"; // name of the file to make or read
     public static final boolean PRINT_DEBUG_INFO      = true;   // true to print info about the recoring and playback
     public static final boolean VERBOSE_DEBUG_PRINT   = true;   // true to print a lot of in depth info about recording and playback
