386ac1b7d895d43549d29a7a0c20923ceb8a99b9
Scott McKool <44004555+Scott-McKool@users.noreply.github.com>
2021-03-08 08:28:59 -0500

    added bools for interpolating and for recording

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -11,5 +11,7 @@ public final class Constants {
     public static final String  SAVE_FILE_NAME        = "test"; // name of the file to make or read
     public static final boolean PRINT_DEBUG_INFO      = true;   // true to print info about the recoring and playback
     public static final boolean VERBOSE_DEBUG_PRINT   = true;   // true to print a lot of in depth info about recording and playback
+    public static final boolean SHOULD_RECORD         = true;   // if false will block the "start recording" method
+    public static final boolean INTERPOLATE_VALUES    = true;   // should data retreived between updates be interpolated (a method of constructing new data points between two given data points)
   }
 }
